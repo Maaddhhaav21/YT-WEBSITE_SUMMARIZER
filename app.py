e567c481dc127f7e6684484cfe03a1e1b349ce40
Madhav Manoj <maadhavmanoj21@gmail.com>
2026-07-31 01:24:08 +0530

    libraries

---
+++ app.py
@@ -0,0 +1,12 @@
+import validators,streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
+
+
+## sstreamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.subheader('Summarize URL')
+
